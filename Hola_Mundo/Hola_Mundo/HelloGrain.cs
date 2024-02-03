be7dc1f1c2fbb406d80938f255d73f97861be6bd
ale <karensontay16@gmail.com>
2024-02-03 17:46:49 -0600

    basics y hello world

---
+++ Hola_Mundo/Hola_Mundo/HelloGrain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo
+{
+    public sealed class HelloGrain : Grain, IHelloGrain
+    {
+        public ValueTask<string> SayHello(string greeting) =>
+            ValueTask.FromResult($"Hello, {greeting}!");
+    }
+}
---
+++ Hola_Mundo/Hola_Mundo/IHelloGrain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo
+{
+    public interface IHelloGrain : IGrainWithStringKey
+    {
+        ValueTask<string> SayHello(string greeting);
+    }
+}
